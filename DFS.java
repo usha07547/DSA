ebad471302e205e6001d34bf41368f03812af41f
IYAPLA USHA <156493279+usha07547@users.noreply.github.com>
2025-08-05 23:05:24 +0530

    Create DFS.java

---
+++ DFS.java
@@ -0,0 +1,52 @@
+class Node {
+    int data;
+    Node left, right;
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+class DFS {
+    void preorder(Node root) {
+        if (root == null) return;
+
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    void inorder(Node root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+    void postorder(Node root) {
+        if (root == null) return;
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        DFS tree = new DFS();
+        System.out.print("Preorder: ");
+        tree.preorder(root);
+        System.out.println();
+
+        System.out.print("Inorder: ");
+        tree.inorder(root);
+        System.out.println();
+
+        System.out.print("Postorder: ");
+        tree.postorder(root);
+        System.out.println();
+    }
+}
